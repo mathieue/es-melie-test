e3b75c241105e821996d7189a19e461d5f122b1f
Mathieu ELIE <mathieu.elie@aquilenet.fr>
2020-03-23 19:44:57 +0100

    search box as you type watching

--- static/js/app.js
+++ static/js/app.js
@@ -1,1 +1,8 @@
 console.log('hello there !');
+
+$(document).ready(function () {
+    $("#search-input").on("input", function () {
+        var val = $(this).val();
+        console.log('you typed: ' + val);
+    });
+});
