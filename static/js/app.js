90cff332fcd621f39dbd17df1cfd9997ed313ee4
Mathieu ELIE <mathieu.elie@aquilenet.fr>
2020-03-24 20:04:46 +0100

    axio get on ping

--- static/js/app.js
+++ static/js/app.js
@@ -1,8 +1,23 @@
 console.log('hello there !');
 
+
+function doSearch(q) {
+    axios.get('/ping', {
+        params: {
+            q: q
+        }
+    }).then(function (response) {
+        console.log(response);
+    }).catch(function (error) {
+        console.log(error);
+    });
+}
+
+
 $(document).ready(function () {
     $("#search-input").on("input", function () {
         var val = $(this).val();
         console.log('you typed: ' + val);
+        doSearch(val);
     });
 });
