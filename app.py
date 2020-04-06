7106f2d97d1c01fadcf26697cda7ec2690667fe1
Mathieu ELIE <mathieu.elie@aquilenet.fr>
2020-04-06 15:09:26 +0200

    managing config and multiple searches

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, jsonify, render_template, request
 from flask_cors import CORS
 from elasticsearch import Elasticsearch
+import os
+import sys
+import yaml
 
 DEBUG = True
 
@@ -13,6 +16,16 @@ app = Flask(__name__)
 
 CORS(app, resources={r'/*': {'origins': '*'}})
 
+
+config_path = os.environ.get('CONFIG_PATH', 'conf')
+sys.path.append(config_path)
+
+from searches import *
+
+with open('%s/config.yml' % config_path) as f:
+    data = yaml.load(f, Loader=yaml.FullLoader)
+    print(data)        
+
 @app.route('/ping', methods=['GET'])
 def ping():
     results = es.cluster.health()
@@ -20,23 +33,20 @@ def ping():
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', searches=data['searches'])
 
 @app.route('/search', methods=['GET'])
 def search():
     q = request.args.get('q')
+    searchid = request.args.get('searchid')
 
-    body = {
-        "query": {
-            "query_string": {
-                "query": q
-            }
-        }
-    }
+    search = data['searches'][int(searchid)]
+    print(search)
 
-    results = es.search(body=body)
-    return jsonify(results)
+    body= getattr(sys.modules[__name__], "dosearch_%s" % search['search'])(q)
 
+    results = es.search(index=search['index'], body=body)
+    return render_template('search-results.html', results=results, showField=search['show-field'])
 
 if __name__ == '__main__':
     app.run()
--- static/js/app.js
+++ static/js/app.js
@@ -1,23 +1,39 @@
 console.log('hello there !');
 
+function showSearchResult(content) {
+    document.getElementById("content-search").innerHTML = content;
+}
 
-function doSearch(q) {
+function doSearch(q, searchSelect) {
     axios.get('/search', {
         params: {
-            q: q
+            q: q,
+            searchid: searchSelect
         }
     }).then(function (response) {
-        console.log(response);
+        showSearchResult(response.data);
     }).catch(function (error) {
         console.log(error);
+        document.getElementById("content-search").innerHTML(error);
     });
 }
 
+function getSearchInputs() {
+    var val = document.getElementById("search-input").value;
+    var e = document.getElementById("searchSelect");
+    var searchSelect = e.options[e.selectedIndex].value;
+
+    console.log('you typed: ' + val + ' search number ' + searchSelect);
+    doSearch(val, searchSelect);
+}
 
 $(document).ready(function () {
     $("#search-input").on("input", function () {
-        var val = $(this).val();
-        console.log('you typed: ' + val);
-        doSearch(val);
+        getSearchInputs();
     });
+
+    $("#searchSelect").change(function () {
+        getSearchInputs();
+    });
+
 });
